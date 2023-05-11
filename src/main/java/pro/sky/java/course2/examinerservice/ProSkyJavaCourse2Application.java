f24e1a47fb8a8d8feda01adff685f5b8ca4748a0
Sergei-Gots <Sergei.Gots@yahoo.com>
2023-05-12 02:36:12 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/examinerservice/ProSkyJavaCourse2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examinerservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProSkyJavaCourse2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProSkyJavaCourse2Application.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/java/course2/examinerservice/ProSkyJavaCourse2ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.examinerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProSkyJavaCourse2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
